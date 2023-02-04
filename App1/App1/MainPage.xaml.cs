5b0c25be787653d1d9f43dcbd2eb4c8778c5cbc8
zubr radel <gggg26705@gmail.com>
2023-02-04 09:52:03 +0300

    Добавьте файлы проекта.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if(Login.Text != "" && Password.Text != "")
+            {
+                await this.Navigation.PushModalAsync(new TabbedPage1());
+            }
+        }
+    }
+}
